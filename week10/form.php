16f4dcabb1487dce83d30cd30bbb749bca1ed4dc
mismailhadi <mismailhadi2006@gmail.com>
2025-12-09 08:24:44 +0700

    Add files via upload

--- week10/form.php
+++ week10/form.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if($_SESSION['isLoggedIn'])
+    if(isset($_SESSION['isLoggedIn']) AND $_SESSION['isLoggedIn']);
     {
         header("Location: home.php");
     }
--- week10/home.php
+++ week10/home.php
@@ -16,5 +16,22 @@
 </head>
 <body>
     <h1>Selamat Datang, <?php echo $_SESSION['username']?> </h1>
+    <a href="logout.php">Logout</a>
+
+    <h4>Daftar User</h4>
+    <table border="1" cellspacing=0 cellpadding=5>
+        <tr>
+            <td>No</td>
+            <td>Username</td>
+            <td>Status</td>
+            <td>Aksi</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 </body>
 </html>
