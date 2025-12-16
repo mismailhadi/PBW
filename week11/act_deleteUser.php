f792909982fb8e8ad1cd009233d36c8b6e96744d
mismailhadi <mismailhadi2006@gmail.com>
2025-12-16 08:36:59 +0700

    Add files via upload

--- week11/act_deleteUser.php
+++ week11/act_deleteUser.php
@@ -1,2 +1,18 @@
 <?php
     session_start();
+    if(!$_SESSION['isLoggedIn'])
+    {
+        header ("Location: form.php");
+    }
+
+    include "koneksi.php";
+    $sql = "DELETE FROM users WHERE id=?";
+    $stmt = $koneksi->prepare($sql);
+    if($stmt->execute([$_GET['id']]))
+    {
+        header("Location: home.php?page=list");
+    }
+    else{
+        echo "<script>alert('User gagal dihapus')
+        history";
+    }
